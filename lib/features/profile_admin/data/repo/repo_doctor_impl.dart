81f4e9c16c33b85cc35fa6d9a4f6252b929fb2d6
Mostafa Esmail <mostafaelshrbeeny98@gmail.com>
2024-12-21 22:39:54 +0200

    integrate get data of doctor

--- lib/features/profile_admin/data/repo/repo_doctor_impl.dart
+++ lib/features/profile_admin/data/repo/repo_doctor_impl.dart
@@ -1,13 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dartz/dartz.dart';
 import 'package:swe_medical/core/erorr/failure.dart';
 import 'package:swe_medical/features/profile_admin/data/model/doctor_model.dart';
 import 'package:swe_medical/features/profile_admin/data/repo/repo_doctor.dart';
 
-class RepoDoctorImpl extends RepoDoctor{
-  @override
-  Future<Either<Failure, DoctorModel>> getProfileData() {
-    // TODO: implement getProfileData
-    throw UnimplementedError();
-  }
+class RepoDoctorImpl extends RepoDoctor {
+  final FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
 
+  @override
+  Future<Either<Failure, DoctorModel>> getProfileData() async {
+    try {
+      final docSnapshot = await firebaseFirestore
+          .collection("doctor")
+          .doc("gTIy17dgtECZvoBR64eA")
+          .get();
+
+      if (docSnapshot.exists) {
+        final patient =
+            DoctorModel.fromJson(docSnapshot.data() as Map<String, dynamic>);
+        return right(patient);
+      } else {
+        return left(ServerFailure("Patient not found"));
+      }
+    } catch (e) {
+      return left(ServerFailure(e.toString()));
+    }
+  }
 }
